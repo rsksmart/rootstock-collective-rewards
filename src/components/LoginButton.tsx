64be4e88119e307090cd90b9081f3c168ada6f8e
rookiecol <juanagudelo@192.168.1.29.non-exists.ptr.local>
2025-01-14 16:42:28 +0700

    fix: cleaning logs in auth logic

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -18,16 +18,13 @@ export const LoginButton = () => {
       chain={rootstockTestnet}
       auth={{
         isLoggedIn: async (address) => {
-          console.log("checking if logged in!", { address });
           return await isLoggedIn();
         },
         doLogin: async (params) => {
-          console.log("logging in!");
           await login(params);
         },
         getLoginPayload: async ({ address }) => generatePayload({ address }),
         doLogout: async () => {
-          console.log("logging out!");
           await logout();
         },
       }}

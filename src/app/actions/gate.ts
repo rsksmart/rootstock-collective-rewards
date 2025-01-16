5182133f117422172c0970d8f999154db26d0862
JuanAgudeloRSL <juan.agudelo@ext.rootstocklabs.com>
2025-01-16 18:17:14 +0700

    fix: simplifying the auth action

--- src/app/actions/gate.ts
+++ src/app/actions/gate.ts
@@ -4,22 +4,13 @@ import { balanceOf } from "thirdweb/extensions/erc20";
 /**
  * Checks if the given address has access to the exclusive content.
  * @param address - The user's wallet address.
- * @returns A boolean indicating if the address has access.
+ * @returns An object containing the `isMember` status and the `amount` of tokens.
  */
-export default async function hasAccess(address: string): Promise<any> {
-  return await isMember(address);
-}
-/**
- * Checks if the given address has access to the exclusive content.
- * @param address - The user's wallet address.
- * @returns A boolean indicating if the address has access.
- */
-async function isMember(address: string): Promise<any> {
+export default async function hasAccess(address: string): Promise<{ isMember: boolean; amount: bigint }> {
   const balance = await balanceOf({
     contract: stRIF,
     address: address,
   });
-
   return {
     amount: balance,
     isMember: balance > 0,
